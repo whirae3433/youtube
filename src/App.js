b0d9e432439bc9e5e632ebae5f41220655dc7e1f
Whirae <whirae3433@gmail.com>
2025-06-20 16:38:05 +0900

    헤더 생성및 스타일링

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import SearchHeader from './pages/SearchHeader';
 
 function App() {
   return (
     <>
+      <SearchHeader />
       <Outlet />
     </>
   );
