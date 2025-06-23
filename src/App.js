0a0acb2ba20c4ed03b19a583948ef88e16fc90aa
Whirae <whirae3433@gmail.com>
2025-06-23 19:07:39 +0900

    context, api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import SearchHeader from './pages/SearchHeader';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { YoutubeApiProvider } from './context/YoutubeApiContext';
 
+const queryClient = new QueryClient();
 function App() {
   return (
     <>
       <SearchHeader />
-      <Outlet />
+      <YoutubeApiProvider>
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+        </QueryClientProvider>
+      </YoutubeApiProvider>
     </>
   );
 }
--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import VideoCard from '../components/VideoCard';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos {keyword? `🔍${keyword}` : '🔥'}</div>;
+  const { youtube } = useYoutubeApi();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ['videos', keyword],
+    queryFn: () => {
+      return youtube.search(keyword);
+    },
+  });
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : '🔥'}</div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Something is wrong 😂</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
